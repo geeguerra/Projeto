fb333396e6127126fbc43317aa57926ba1f2c947
joaoprimo7 <joao.v.primo@aluno.senai.br>
2026-05-20 08:59:56 -0300

    CadastroFunc

--- Produto/produto.js
+++ Produto/produto.js
@@ -4,4 +4,14 @@ const API_URL ='http://localhost:5146/api';
 
 //Mostrar mensagem de ERRO ou SUCESSO no Login e no Registro
 
-const messageBox = document.getElementById
+const messageBox = document.getElementById ('message-box');
+
+function mostrarMensagem(texto, type='error'){  
+ if(!messageBox)return;
+  messageBox.textContent = texto;
+    messageBox.className = type ==='error'? 'msg-error' : 'msg-sucess';
+    messageBox.style.display = 'block';
+    setTi
+    meout(() =>{messageBox.style.display = 'nome';}, 4000);
+    
+}
